e6254fda7b9b5a90c3c88e36614a7b533b0dbf8e
yemliha81 <yemlihaddln@hotmail.com>
2020-02-01 21:38:45 +0300

    some fixes

--- application/controllers/Creator.php
+++ application/controllers/Creator.php
@@ -22,7 +22,7 @@ class Creator extends CI_Controller {
 		
 		$post = $this->input->post();
 		$controller_name = $post['table_name'];
-
+		$table_name = strtolower($post['table_name']);
 
 		$query .= "CREATE TABLE IF NOT EXISTS ".$post['table_name']."_table (
 					id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,";
@@ -42,16 +42,13 @@ class Creator extends CI_Controller {
 		$query = substr($query, 0, -1);
 		$query .= " ) ;";
 		
-
-
-
 		$run = $this->db->query($query);
 
 		if($run){
 			
 			$fileName = "application/controllers/".$post['table_name'].".php";
 			$this->create_controller($controller_name, $fileName);
-			
+			$this->create_views($table_name);
 			
 			die("Successful!!!");
 		}else{
@@ -60,19 +57,61 @@ class Creator extends CI_Controller {
 
 	}
 
+	public function create_views($table_name){
+		
+		$table_name_list_view = strtolower($table_name.'_list_view');
+		$table_name_add_view = strtolower($table_name.'_add_view');
+		$table_name_update_view = strtolower($table_name.'_update_view');
+		
+		
+		
+		$views = array( $table_name_list_view , $table_name_add_view, $table_name_update_view );
+
+		foreach($views as $view){
+			$fileName = "application/views/".$view.".php";
+			if(!file_exists($fileName)){
+			
+				touch($fileName);
+				$fp = fopen( $fileName, 'w');
+				fwrite($fp, $this->file_content_creator("view", $view));
+				fclose($fp);
+				
+			}
+		}
+
+		
+	}
+
 	public function create_controller($controller_name, $fileName){
 		if(!file_exists($fileName)){
 			
 			touch($fileName);
 			$fp = fopen( $fileName, 'w');
-			fwrite($fp, $this->file_content_creator($controller_name));
+			fwrite($fp, $this->file_content_creator("controller", $controller_name));
 			fclose($fp);
 			
 		}
 	}
 
-	public function file_content_creator($table_name){
+	public function file_content_creator($type, $table_name){
 
+		if($type == "controller"){
+			return $this->controller_content($table_name);
+		}
+
+		if($type == "view"){
+			return $this->view_content($table_name);
+		}
+		
+
+	}
+
+	public function view_content($view_name){
+		return "This is ".$view_name." content .......";
+	}
+
+	public function controller_content($table_name){
+		
 		$db_table_name = strtolower($table_name.'_table');
 		$table_name_list = strtolower($table_name.'_list');
 		$table_name_list_view = strtolower($table_name.'_list_view');
@@ -87,49 +126,47 @@ class Creator extends CI_Controller {
 		$content = "<?php
 		defined('BASEPATH') OR exit('No direct script access allowed');
 		
-		class $table_name extends CI_Controller {
+			class $table_name extends CI_Controller {
 
-			public function $table_name_list()
-			{
-				\$data['item_list'] = \$this->db->select('*')->get('$db_table_name')->result_array();
-				debug(\$data['item_list']);
-				\$this->load->view('$table_name_list_view', \$data);
-			}
+				public function $table_name_list()
+				{
+					\$data['item_list'] = \$this->db->select('*')
+						->get('$db_table_name')->result_array();
+					
+					\$this->load->view('$table_name_list_view', \$data);
+				}
 
-			public function $table_name_add()
-			{
-				\$this->load->view('$table_name_add_view', \$data);
-			}
+				public function $table_name_add()
+				{
+					\$this->load->view('$table_name_add_view', \$data);
+				}
 
-			public function $table_name_add_post()
-			{
-				//INSERT process will be here
-			}
+				public function $table_name_add_post()
+				{
+					//INSERT process will be here
+				}
 
-			public function $table_name_update()
-			{
-				\$this->load->view('$table_name_update_view', \$data);
-			}
+				public function $table_name_update()
+				{
+					\$this->load->view('$table_name_update_view', \$data);
+				}
 
-			public function $table_name_update_post()
-			{
-				//UPDATE process will be here
-			}
+				public function $table_name_update_post()
+				{
+					//UPDATE process will be here
+				}
 
-			public function $table_name_delete()
-			{
-				//DELETE process will be here
+				public function $table_name_delete()
+				{
+					//DELETE process will be here
+				}
+				
 			}
-			
-		}
 
 		?>";
 
 		return strval($content);
-
 	}
-
-
 
 
 
--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -1,40 +1,41 @@
 <?php
 		defined('BASEPATH') OR exit('No direct script access allowed');
 		
-		class Product extends CI_Controller {
+			class Product extends CI_Controller {
 
-			public function Product_list()
-			{
-				$data['item_list'] = $this->db->select('*')->get('product_table')->result_array();
-				debug($data['item_list']);
-				$this->load->view('Product_list_view', $data);
-			}
+				public function product_list()
+				{
+					$data['item_list'] = $this->db->select('*')
+						->get('product_table')->result_array();
+					
+					$this->load->view('product_list_view', $data);
+				}
 
-			public function Product_add()
-			{
-				$this->load->view('Product_add_view', $data);
-			}
+				public function product_add()
+				{
+					$this->load->view('product_add_view', $data);
+				}
 
-			public function Product_add_post()
-			{
-				//INSERT process will be here
-			}
+				public function product_add_post()
+				{
+					//INSERT process will be here
+				}
 
-			public function Product_update()
-			{
-				$this->load->view('Product_update_view', $data);
-			}
+				public function product_update()
+				{
+					$this->load->view('product_update_view', $data);
+				}
 
-			public function Product_update_post()
-			{
-				//UPDATE process will be here
-			}
+				public function product_update_post()
+				{
+					//UPDATE process will be here
+				}
 
-			public function Product_delete()
-			{
-				//DELETE process will be here
+				public function product_delete()
+				{
+					//DELETE process will be here
+				}
+				
 			}
-			
-		}
 
 		?>
